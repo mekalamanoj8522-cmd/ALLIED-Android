5564c0884ddcca389d2212723240b1877b4c9a1e
mekalamanoj8522-cmd <mekalamanoj8522@gmail.com>
2026-08-22 23:13:26 +0530

    Create PlayerService.java

---
+++ app/src/main/java/com/allied/music/PlayerService.java
@@ -0,0 +1,66 @@
+package com.allied.music;
+
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.MediaMetadata;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.session.MediaSession;
+import androidx.media3.session.MediaSessionService;
+
+public class PlayerService extends MediaSessionService {
+
+    private ExoPlayer player;
+    private MediaSession mediaSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        player = new ExoPlayer.Builder(this).build();
+
+        MediaItem mediaItem =
+                new MediaItem.Builder()
+                        .setUri(
+                            "https://mekalamanoj8522-cmd.github.io/sample.mp3"
+                        )
+                        .setMediaMetadata(
+                                new MediaMetadata.Builder()
+                                        .setTitle("ALLIED Music")
+                                        .setArtist("ALLIED")
+                                        .setAlbumTitle("ALLIED")
+                                        .build()
+                        )
+                        .build();
+
+        player.setMediaItem(mediaItem);
+
+        mediaSession =
+                new MediaSession.Builder(this, player)
+                        .build();
+
+        player.prepare();
+        player.play();
+    }
+
+    @Override
+    public MediaSession onGetSession(
+            MediaSession.ControllerInfo controllerInfo) {
+
+        return mediaSession;
+    }
+
+    @Override
+    public void onDestroy() {
+
+        if (mediaSession != null) {
+            mediaSession.release();
+            mediaSession = null;
+        }
+
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+
+        super.onDestroy();
+    }
+}
